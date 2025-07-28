e15fdbb09d0ec387803298612b2ea1fe7555a438
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2025-07-28 17:40:54 +0700

    change topK to 100 from 1000

--- utils/ask_handler.js
+++ utils/ask_handler.js
@@ -50,7 +50,7 @@ const askHandler = async (question, userId) => {
         const manual = await collection.query({
             queryEmbeddings: [queryEmbedding],
             where: { userId: "default"},
-            nResults: 20,
+            nResults: 1000,
             include: ["documents", "metadatas", "distances"],
         });
 
@@ -64,7 +64,7 @@ const askHandler = async (question, userId) => {
 
         const combined = allMatches
             .sort((a, b) => a.distance - b.distance)
-            .slice(0, 20);
+            .slice(0, 100);
 
         const context = combined.map((item) => item.document).join("\n\n");
         const sources = combined
